4f436c69d05ff097de16df7bc73793e1e848c4ee
wangrumeng <wangrmz202095@163.com>
2023-12-11 22:31:15 +0800

    feature:gateway==revert

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1、开始
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // 排除数据源相关的配置
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
